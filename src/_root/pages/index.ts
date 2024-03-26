f69be795b63a80de711828cd0ef2042ec5d0ed43
Taylor <minhtan14122013@gmail.com>
2024-03-27 00:28:28 +0700

    - Xóa những file không cần thiết.

--- src/_root/pages/index.ts
+++ src/_root/pages/index.ts
@@ -1,5 +1,5 @@
 export { default as Home } from "./Home";
-export { default as CreatePost } from "./CreateContribution";
+export { default as CreatePost } from "./CreatePost";
 export { default as Profile } from "./Profile";
 export { default as UpdateProfile } from "./UpdateProfile";
 export { default as EditPost } from "./EditPost";
