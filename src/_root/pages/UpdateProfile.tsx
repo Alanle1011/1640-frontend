ffa6a2de654b3116813e1131529bb46a355df277
MarkTaylor1412 <minhtan14122013@gmail.com>
2024-04-21 19:01:09 +0700

    - DONE.

--- src/_root/pages/UpdateProfile.tsx
+++ src/_root/pages/UpdateProfile.tsx
@@ -9,10 +9,10 @@ const UpdateProfile : React.FC<{ userData: ILoginUser }> = ({ userData }) => {
             <div className="common-container">
                 <div className="max-w-5xl flex-start gap-3 justify-start w-full">
                     <img
-                        src="/assets/icons/add-post.svg"
+                        src="/assets/icons/user-edit.svg"
                         width={36}
                         height={36}
-                        alt="add"
+                        alt="edit"
                     />
                     <h2 className="h3-bold md:h2-bold text-left w-full">Edit User</h2>
                 </div>
